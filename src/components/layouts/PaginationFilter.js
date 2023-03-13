25ff87b51daf5b01c92ccd059508f78efc8830f8
rayan <rhrayan2228@gmail.com>
2023-03-13 16:54:50 +0600

    sidebar filter responsive

--- src/components/layouts/PaginationFilter.js
+++ src/components/layouts/PaginationFilter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PaginationFilter = ({ id, title, children, className, onChange }) => {
   return (
-    <div className="flex-col sm:flex-row flex items-start sm:items-center sm:gap-x-3 gap-x-0">
+    <div className="flex flex-col items-start sm:flex-row sm:items-center sm:gap-x-3 gap-x-0">
       <label for={id}>{title}</label>
       <select
         onChange={onChange}
@@ -11,6 +11,7 @@ const PaginationFilter = ({ id, title, children, className, onChange }) => {
       >
         {children}
       </select>
+    
     </div>
   );
 };
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Breadcrumbs from "../components/layouts/Breadcrumbs";
 import Container from "../components/layouts/Container";
 import Flex from "../components/layouts/Flex";
@@ -6,28 +6,78 @@ import Option from "../components/layouts/Option";
 import Pagination from "../components/layouts/Pagination";
 import PaginationFilter from "../components/layouts/PaginationFilter";
 import LeftSideBar from "../components/LeftSideBar";
-
+import { MdFilterAlt } from "react-icons/md";
+import { RiFilterOffFill } from "react-icons/ri";
+import { useRef } from "react";
 
 const Shop = () => {
   const [pageShow, setPageShow] = useState(12);
+  const [sideBarShow, setSideBarShow] = useState(false);
+  const filterOnRef = useRef();
+  const filterOffRef = useRef();
+
   const handleChange = (e) => {
     setPageShow(+e.target.value);
   };
+  useEffect(() => {
+    function resize() {
+      if (window.innerWidth < 1024) {
+        setSideBarShow(false);
+      } else {
+        setSideBarShow(true);
+      }
+    }
+    resize();
+    window.addEventListener("resize", resize);
+    document.body.addEventListener("click", (e) => {
+      if (filterOnRef.current.contains(e.target)) {
+        setSideBarShow(true);
+      } else{
+        setSideBarShow(false);
+      }
+      // else if (e.target.parentElement == filterOffRef.current) {
+      //   setSideBarShow(true);
+      // }else{
+      //   setSideBarShow(false);
+      // }
+      // if (e.target.parentElement == filterOffRef.current) {
+      //   console.log("dfdf");
+      // }
+    });
+  }, [sideBarShow]);
   return (
     <section className="sm:mt-[124px] mt-[50px]">
       <Container>
         <Breadcrumbs
           link1="/"
           value1="home"
-          link2="/products"
+          link2="/{window.location.pathname.split('/')[1]}"
           value2={window.location.pathname.split("/")[1]}
         />
-        <Flex className="flex gap-x-10 sm:mt-[130px] mt-[80px]">
-          <div className=" w-[25%]">
-            <LeftSideBar/>
-          </div>
-          <div className=" w-[75%] relative">
+        <Flex className="flex gap-x-10 sm:mt-[130px] mt-[80px] relative">
+          {sideBarShow && (
+            <div
+              className="w-full md:w-[25%] absolute top-0 left-0 z-50 md:static md:block bg-[#fcfcfc] p-2.5 md:bg-transparent"
+              ref={filterOffRef}
+            >
+              <p
+                className="flex items-center p-2.5 text-white bg-black md:hidden cursor-pointer mb-2"
+                onClick={() => setSideBarShow(false)}
+              >
+                Close Filter <RiFilterOffFill />
+              </p>
+              <LeftSideBar />
+            </div>
+          )}
+          <div className=" md:w-[75%] relative w-full">
             <div className="flex flex-col sm:flex-row sm:gap-x-10 sm:gap-y-0 gap-y-3 sm:mb-16 mb-3 font-secondary font-normal text-base text-[#767676] capitalize justify-end ">
+              <p
+                className="flex items-center p-2.5 text-white bg-black md:hidden cursor-pointer "
+                onClick={() => setSideBarShow(true)}
+                ref={filterOnRef}
+              >
+                Filter <MdFilterAlt />
+              </p>
               <PaginationFilter
                 title="short by:"
                 id="shortBy"
@@ -48,7 +98,6 @@ const Shop = () => {
                 <Option option="48" value="48" />
               </PaginationFilter>
             </div>
-
             <Pagination itemsPerPage={pageShow} />
           </div>
         </Flex>
